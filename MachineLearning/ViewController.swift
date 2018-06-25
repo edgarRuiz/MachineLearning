b15cbf105815bcc0822c048408b94caa97979d34
Edgar Ruiz <edgarr@openvpn-client.tawny.brandwatch.com>
2018-06-25 06:42:02 -0400

    First Commit

--- MachineLearning/ViewController.swift
+++ MachineLearning/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var uiImageView: UIImageView!
+    var imagePicker = UIImagePickerController();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self;
+        imagePicker.sourceType = .photoLibrary;
+        imagePicker.allowsEditing = false;
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage  = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            uiImageView.image = userPickedImage;
+        }
+        dismiss(animated:true, completion: nil)
 
-
+    }
+    
 }
 
