6b8bce2bacf242f3e7dc038bc7581c954929878a
Edgar Ruiz <edgarr@client.openvpn.net>
2018-06-25 20:22:53 -0400

    Finish Hot dog app with CoreML

--- MachineLearning/ViewController.swift
+++ MachineLearning/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
@@ -32,9 +34,43 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage  = info[UIImagePickerControllerOriginalImage] as? UIImage{
             uiImageView.image = userPickedImage;
+            
+            guard let ciImage = CIImage(image: userPickedImage) else{
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            detect(ciImage: ciImage);
+            print()
         }
         dismiss(animated:true, completion: nil)
 
+    }
+    
+    func detect(ciImage : CIImage){
+        print("here")
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Loading CoreML Model failed")
+        }
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                guard let results = request.results as? [VNClassificationObservation] else{
+                    fatalError("Model failed to process image")
+                }
+                if let firstItem = results.first{
+                    if firstItem.identifier.contains("hot dog"){
+                        self.navigationItem.title = "Hot dog indeed!!"
+                    }else{
+                        self.navigationItem.title = "Not a hot dog!"
+                    }
+                }
+            }
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage);
+        
+        do{
+            try handler.perform([request]);
+        }catch{
+            print(error)
+        }
+    
     }
     
 }
